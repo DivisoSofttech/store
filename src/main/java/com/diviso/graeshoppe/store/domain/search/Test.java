9a0002d3310f02cf3a5e44d3452e484bb39c9111
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-12-31 13:26:26 +0530

    c1

---
+++ src/main/java/com/diviso/graeshoppe/store/domain/search/Test.java
@@ -0,0 +1,109 @@
+package com.diviso.graeshoppe.store.domain.search;
+import  org.springframework.data.elasticsearch.annotations.CompletionField;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.GeoPointField;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.data.elasticsearch.core.completion.Completion;
+/**
+ * A Store.
+ */
+
+@org.springframework.data.elasticsearch.annotations.Document(indexName = "test")
+public class Test implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    
+    @org.springframework.data.elasticsearch.annotations.Field(type = FieldType.Keyword)
+    private Long id;
+
+  
+    @Column(name = "name")
+    private String name;
+
+    @CompletionField(maxInputLength = 100)
+    private Completion suggest;
+
+
+   
+
+    public String getName() {
+        return name;
+    }
+
+  
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+  
+  
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+ 
+    @Override
+	public String toString() {
+		return "Test [id=" + id + ", name=" + name + ", suggest=" + suggest + "]";
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public Completion getSuggest() {
+		return suggest;
+	}
+
+
+
+	public void setSuggest(Completion suggest) {
+		this.suggest = suggest;
+	}
+
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+
+	@Override
+    public int hashCode() {
+        return 31;
+    }
+
+  /*  @Override
+    public String toString() {
+        return "Store{" +
+            "id=" + getId() +
+            ", regNo='" + getRegNo() + "'" +
+            ", name='" + getName() + "'" +
+            ", info='" + getInfo() + "'" +
+            ", minAmount=" + getMinAmount() +
+            ", maxDeliveryTime='" + getMaxDeliveryTime() + "'" +
+            "}";
+    }*/
+}
